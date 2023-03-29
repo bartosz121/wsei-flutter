49b3de4d4e7198f396473e6ace8d100b5a3d5605
bartosz121 <bmagiera121@gmail.com>
2023-03-29 21:18:19 +0200

    Add icon to change trending view - list/grid

--- lib/views/trending_view.dart
+++ lib/views/trending_view.dart
@@ -15,6 +15,7 @@ class _MoviesViewState extends State<MoviesView> {
   final api = ApiService();
 
   late Future<List<TrendingMovie>> _movies;
+  bool isGridMode = false;
 
   @override
   void initState() {
@@ -26,73 +27,131 @@ class _MoviesViewState extends State<MoviesView> {
     return await api.getTrendingMovies();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Movies'),
-      ),
-      body: FutureBuilder<List<TrendingMovie>>(
-        future: _movies,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                final movie = snapshot.data![index];
-                return Card(
-                  elevation: 5,
-                  child: SizedBox(
-                    width: 300,
-                    child: Row(
+  Widget _buildList(List<TrendingMovie> movies) {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: movies.length,
+      itemBuilder: (context, index) {
+        final movie = movies[index];
+        return Card(
+          elevation: 5,
+          child: SizedBox(
+            width: 300,
+            child: Row(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: InkWell(
+                        onTap: () {
+                          Get.to(() => MovieDetails(movieId: movie.id),
+                              transition: Transition.fade,
+                              duration: const Duration(seconds: 1));
+                        },
+                        child: Image.network(
+                          'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                          fit: BoxFit.cover,
+                          width: 150,
+                        ))),
+                const SizedBox(width: 8.0),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: InkWell(
-                                onTap: () {
-                                  Get.to(() => MovieDetails(movieId: movie.id),
-                                      transition: Transition.fade,
-                                      duration: const Duration(seconds: 1));
-                                },
-                                child: Image.network(
-                                  'https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                                  fit: BoxFit.cover,
-                                  width: 150,
-                                ))),
-                        const SizedBox(width: 8.0),
-                        Expanded(
-                          child: Container(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  movie.title,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                                const SizedBox(height: 8.0),
-                                Text(
-                                  movie.overview,
-                                  style: const TextStyle(
-                                    color: Colors.grey,
-                                    fontSize: 14.0,
-                                  ),
-                                ),
-                              ],
-                            ),
+                        Text(
+                          movie.title,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          movie.overview,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14.0,
                           ),
                         ),
                       ],
                     ),
                   ),
-                );
-              },
-            );
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildGrid(List<TrendingMovie> movies) {
+    return GridView.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 16,
+      crossAxisSpacing: 16,
+      padding: const EdgeInsets.all(16),
+      children: movies
+          .map((movie) => GestureDetector(
+                onTap: () {
+                  Get.to(() => MovieDetails(movieId: movie.id),
+                      transition: Transition.fade,
+                      duration: const Duration(seconds: 1));
+                },
+                child: Card(
+                  elevation: 5,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: Image.network(
+                          'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                          fit: BoxFit.fitHeight,
+                          height: 400,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        movie.title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ))
+          .toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Movies'),
+        actions: [
+          IconButton(
+            icon: Icon(isGridMode ? Icons.list : Icons.grid_on),
+            onPressed: () {
+              setState(() {
+                isGridMode = !isGridMode;
+              });
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder<List<TrendingMovie>>(
+        future: _movies,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return isGridMode
+                ? _buildGrid(snapshot.data!)
+                : _buildList(snapshot.data!);
           } else if (snapshot.hasError) {
             return const Center(
               child: Text('Error fetching movies'),
